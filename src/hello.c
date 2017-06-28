a30dec5b775ea0601ae63e3f632a02e2663663ce
lijq <lijq@newlandpayment.com>
2017-06-28 15:13:11 +0800

    first init

---
+++ src/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int hello(const char *promp)
+{
+	printf("HELLO: %s\n", promp);
+	return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "hello.h"
+
+
+int main(int argc, char *argv[])
+{
+	hello("hello world!");
+	return 0;
+}
